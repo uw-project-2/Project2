8a8a964ed600ab4535bad2fafdfa28f62379489c
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-04-01 16:26:18 -0700

    minor error in ingredient-api-routes.js

--- routes/ingredient-api-routes.js
+++ routes/ingredient-api-routes.js
@@ -4,9 +4,9 @@ module.exports = function(app) {
   app.get("/api/ingredients", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
+    // In this case, just db.Recipe
     db.Ingredient.findAll({
-      include: [db.Post]
+      include: [db.Recipe]
     }).then(function(dbIngredient) {
       res.json(dbIngredient);
     });
@@ -15,12 +15,12 @@ module.exports = function(app) {
   app.get("/api/ingredients/:id", function(req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
+    // In this case, just db.Recipe
     db.Ingredient.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Post]
+      include: [db.Recipe]
     }).then(function(dbIngredient) {
       res.json(dbIngredient);
     });
