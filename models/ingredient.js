730591d22c8c5f2bf6d40998e15bc741cf154d22
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-04-01 20:58:08 -0700

    working Travis(I hope)

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,17 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
-    var Ingredient = sequelize.define("Ingredient", {
-      // Giving the Ingredient model a name of type STRING
-      name: DataTypes.STRING
+  var Ingredient = sequelize.define("Ingredient", {
+    // Giving the Ingredient model a name of type STRING
+    name: DataTypes.STRING
+  });
+
+  Ingredient.associate = function(models) {
+    // Associating Ingredient with Recipes
+    // When an Ingredient is deleted, also delete any associated Recipes
+    Ingredient.hasMany(models.Post, {
+      onDelete: "cascade"
     });
-  
-    Ingredient.associate = function(models) {
-      // Associating Ingredient with Recipes
-      // When an Ingredient is deleted, also delete any associated Recipes
-      Ingredient.hasMany(models.Post, {
-        onDelete: "cascade"
-      });
-    };
-  
-    return Ingredient;
   };
-  
+
+  return Ingredient;
+};
