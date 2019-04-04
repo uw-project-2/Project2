e166a179c0c349454880ee6d1a3801a1b985d74d
Lacey Gibbons <lazyrider@D-10-19-202-205.dhcp4.washington.edu>
2019-04-03 19:42:03 -0700

    initial

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 var keys = require("../config/keys.js");
 var connection;
 
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -15,10 +16,11 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
+
 connection.connect(function (err) {
   if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+    console.error("error connecting: " + err.stack);
+    return;
   };
   console.log("connected as id: " + connection.threadId);
 });
