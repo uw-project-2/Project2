80af0dfbc917aa0f26acf78774a6b9e86e30cef8
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-04-06 14:00:08 -0700

    line space

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,12 +14,11 @@ module.exports = function(app) {
   app.get("/recipe/:id", function(req, res) {
     db.Recipe.findOne({ where: { id: req.params.id } }).then(function(
       dbRecipe
-    ) { 
+    ) {
       console.log(dbRecipe.ingredients);
       //create a sequelize where condition query to build an array of keys with ingredient IDs in the recipe
 
-      //run another loop through all ingredients to add the name property 
-
+      //run another loop through all ingredients to add the name property
 
       res.render("example", {
         recipe: dbRecipe
