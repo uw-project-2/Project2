6801c598bd40d3eecac497d55ca389d5004c3b8b
tochwat <tochwat@gmail.com>
2019-04-08 10:39:31 -0700

    initial add another ingredient button working on front end

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,77 @@
 /* eslint-disable */
 
 $(document).ready(function () {
-  // console.log("ready!");
+  console.log("ready!");
 
+  // Get references to page elements and declare global ariables
+  var $name = $("#name");
+  var $amount = $("#amount");
+  var $directions = $("#directions");
+  var $submitBtn = $("#submit-btn");
+  var $recipeList = $("#recipe-list");
+  var $ingredient;
+
+  //========= AJAX call to create the ingredients dropdown in the "new recipe" form ==========//
   $.ajax({
     type: "GET",
     url: "/api/ingredients"
   }).then(function (ingredients) {
     // console.log("Making AJAX call");
     var options = ingredients.map(function (ingredient) {
-      return `<option value="${ingredient.name}">${ingredient.name}</option>`;
+      // return `<option id="ingredient-select" value="${ingredient.name}">${ingredient.id}</option>`;
+      return `<option id="ingredient-select" value="${ingredient.id}">${ingredient.name}</option>`;
     });
     $("#ingredients").append(`<input class="form-control" aria-describedby="ingredients" list="options" id="ingredients-dropdown"></input>`);
     $("#ingredients-dropdown").after(`<datalist id="options">${options}</datalist>`);
+
+    $ingredient = $("#ingredients-dropdown");
   });
 
-  // Get references to page elements
-  var $name = $("#name");
-  var $ingredients = $("#ingredients");
-  var $directions = $("#directions");
-  var $submitBtn = $("#submit-btn");
-  var $recipeList = $("#recipe-list");
+
+  //JS code to allow you to add multiple ingredients
+  var next = 0;
+    $("#add-more").click(function(e){
+        e.preventDefault();
+        var addto = "#field" + next;
+        var addRemove = "#field" + (next);
+        next = next + 1;
+        var newIn = ' <div id="field'+ next +'" name="field'+ next +'"><label for="ingredients">Ingredient:</label><div id="ingredients'+ next +'"></div><label for="amount">Ingredient amount:</label><input type="text" class="form-control" id="amount" aria-describedby="amount" placeholder="The amount for this ingredient">';
+        var newInput = $(newIn);
+        // var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >Remove</button></div></div><div id="field">';
+        // var removeButton = $(removeBtn);
+        $(addto).after(newInput);
+        // $(addRemove).after(removeButton);
+        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(next);  
+        
+            // $('.remove-me').click(function(e){
+            //     e.preventDefault();
+            //     var fieldNum = this.id.charAt(this.id.length-1);
+            //     var fieldID = "#field" + fieldNum;
+            //     $(this).remove();
+            //     $(fieldID).remove();
+            // });
+        
+        //========= another AJAX call to create the next ingredients dropdown in the "new recipe" form ========//
+        $.ajax({
+          type: "GET",
+          url: "/api/ingredients"
+        }).then(function (ingredients) {
+          // console.log("Making AJAX call");
+          var options = ingredients.map(function (ingredient) {
+            // return `<option id="ingredient-select" value="${ingredient.name}">${ingredient.id}</option>`;
+            return `<option id="ingredient-select" value="${ingredient.id}">${ingredient.name}</option>`;
+          });
+          $("#ingredients"+ next).append(`<input class="form-control" aria-describedby="ingredients" list="options" id="ingredients-dropdown"></input>`);
+          $("#ingredients-dropdown").after(`<datalist id="options">${options}</datalist>`);
+
+          $ingredient = $("#ingredients-dropdown");
+        });
+
+    });
+
+
+
 
   // The API object contains methods for each kind of request we'll make
   var API = {
@@ -77,14 +128,37 @@ $(document).ready(function () {
     });
   };
 
-  // handleFormSubmit is called whenever we submit a new recipe
+  // handleFormSubmit is called whenever we submit a new recipe ==========================//
   // Save the new recipe to the db and refresh the list
   var handleFormSubmit = function (event) {
     event.preventDefault();
 
+    //find the corresponding key for the selected ingredient
+    console.log("PRINTING INGREDIENT VALUE");
+    console.log($ingredient.val());
+    var ingredientSelect = $ingredient.val();
+
+    console.log("PRINTING AMOUNT");
+    console.log($amount.val());
+
+    var ingredientObj = {
+      ingredients: ingredientSelect,
+      amount: $amount.val()
+    };
+
+    //create ingredients array to push the ingredient object into
+    var ingredientsArray = [];
+    ingredientsArray.push(ingredientObj);
+
+
+    //FIXME: Ingredient IDs for new recipes are store as strings, not integers, so the backend code that JSON parses the recipe info to display on the recipe/example page needs to parseInt the ID?
+
+    //stringify the ingredient array
+    var ingredientString = JSON.stringify(ingredientsArray);
+
     var recipe = {
       recipe_name: $name.val().trim(),
-      ingredients: $ingredients.val().trim(),
+      ingredients: ingredientString,
       directions: $directions.val().trim()
     };
 
@@ -98,7 +172,8 @@ $(document).ready(function () {
     });
 
     $name.val("");
-    $ingredients.val("");
+    $ingredient.val("");
+    $amount.val("");
     $directions.val("");
   };
 
